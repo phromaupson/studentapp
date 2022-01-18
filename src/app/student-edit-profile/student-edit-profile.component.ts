0b82823f2e10fc2753d5f4e8e454b341660b3340
kamkonphrom <kamkonphrom@gmail.com>
2022-01-18 18:17:36 +0700

    add edit function

--- src/app/student-edit-profile/student-edit-profile.component.ts
+++ src/app/student-edit-profile/student-edit-profile.component.ts
@@ -1,15 +1,71 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-student-edit-profile',
   templateUrl: './student-edit-profile.component.html',
-  styleUrls: ['./student-edit-profile.component.css']
+  styleUrls: ['./student-edit-profile.component.css'],
 })
 export class StudentEditProfileComponent implements OnInit {
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private http: HttpClient,
+    private router: Router,
+    private toast: ToastrService
+  ) {}
 
-  constructor() { }
+  public name: String = '';
+  public room: String = '';
+  public mobile: String = '';
+  public description: String = '';
+
+  oldName!: String;
+  user: any;
 
   ngOnInit(): void {
+    this.oldName = this.activatedRoute.snapshot.params['name'];
+    this.getOneUser();
+
+    this.name = this.user.name;
   }
 
+  getOneUser = async () => {
+    await this.http
+      .get(`http://localhost:9000/api/${this.oldName}`)
+      .subscribe((result: any) => {
+        this.name = result.name;
+        this.room = result.room;
+        this.mobile = result.mobile;
+        this.description = result.description;
+        this.user = result;
+      });
+  };
+
+  UpdateUser() {
+    console.log(this.name, this.mobile, this.room, this.description);
+
+    this.http
+      .put(`http://localhost:9000/api/update/${this.oldName}`, {
+        room: this.room,
+        mobile: this.mobile,
+        description: this.description,
+      })
+      .subscribe((result: any) => {
+        this.toast.success(`${this.oldName} Updated`, 'Success');
+        this.router.navigateByUrl('/');
+      });
+
+    // this.http
+    //   .put(`http://localhost:9000/api/update/${this.oldName}`, {
+    //     room: this.room,
+    //     mobile: this.mobile,
+    //     description: this.description,
+    //   })
+    //   .subscribe((msg: any) => {
+    //     this.toast.success(`${this.oldName} Updated`, 'Success');
+    //     this.router.navigateByUrl('/');
+    //   });
+  }
 }
